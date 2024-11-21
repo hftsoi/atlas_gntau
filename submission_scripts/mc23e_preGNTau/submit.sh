3c0bfa12e856fb403feef38e00b28d263b49a047
Ho-Fung Tsoi <hftsoi0411@gmail.com>
2024-11-21 18:12:10 +0100

    wrong place for --extOutFile

--- submission_scripts/mc23e_preGNTau/submit.sh
+++ submission_scripts/mc23e_preGNTau/submit.sh
@@ -4,5 +4,5 @@ pathena --trf='Reco_tf.py --CA --athenaopts="all:--threads=$ATHENA_CORE_NUMBER -
 flags.Trigger.triggerMenuSetup=\"Dev_pp_run3_v1_TriggerValidation_prescale\"; from AthenaMonitoring.DQConfigFlags import allSteeringFlagsOff; \
 allSteeringFlagsOff(flags); flags.DQ.Steering.doHLTMon=True; flags.DQ.Steering.HLT.doTau=True; flags.DQ.Steering.doDataFlowMon=True" \
 --preInclude "all:Campaigns.MC23e" --postInclude "PyJobTransforms.UseFrontier" --steering "doRDO_TRIG" \
---inputRDOFile %IN --outputAODFile %OUT.AOD.pool.root.1 --outputHISTFile %OUT.HIST.pool.root.1 --extOutFile %OUT.EXT.pool.root.1' \
---nCore 2 --noEmail --nFilesPerJob 1 --inOutDsJson jobs_datasets_mc23e.json --nFiles 8 --dumpJson jobs_submit_mc23e.json --inTarBall tarball.tgz 
+--inputRDOFile %IN --outputAODFile %OUT.AOD.pool.root.1 --outputHISTFile %OUT.HIST.pool.root.1' \
+--nCore 2 --noEmail --nFilesPerJob 1 --inOutDsJson jobs_datasets_mc23e.json --nFiles 8 --dumpJson jobs_submit_mc23e.json --extOutFile='expert-monitoring.root' --inTarBall tarball.tgz 
